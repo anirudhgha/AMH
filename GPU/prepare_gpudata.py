71a877d0a045501b17cbd01045a434020a6f5a5f
Lakshmi Anirudh Ghantasala <alasghantasala@gmail.com>
2023-10-19 11:05:39 -0400

    updated file to prepare data for GPU runs

--- GPU/prepare_gpudata.py
+++ GPU/prepare_gpudata.py
@@ -1,5 +1,3 @@
-# Arranges all the data into a single dict, and writes it to a csv file to be used as input to the GPU. 
-
 import time
 import numpy as np
 import mdtraj as md
@@ -15,147 +13,132 @@ from pseudo_fpga import fpga
 
 # ###########################################################################################
 # general MCMC Parameters
-noise_var_range = [10, 1]
-cycles = 40
-segment_sizes = [10, 40, 82]
-iterations = [20, 25, 150]
+noise_var_range = [10, 4]
+cycles = 10
+segment_sizes = [10000, 16]
+iterations = [50, 50]
 beta = 1
-n_runs = 10 # number of repetitions of experiment
+n_runs_per_protein = 1 # number of repetitions of experiment
+n_proposal_limit = 100000 # if 0, run for cycles, else run until n_proposal_limit is reached. 
 
-# protein Specific Details
-dist_filename = r'../T0955/contacts/T0955.pickle' 
-initial_state_pdb_filename = r'../T0955/T0955_unfolded.pdb'
+# 2653, 2847, 2689, 2859, 2763, 2859, 2712
+# protein Specific Details (will search in file system, which is ../protein_name/etc...)
+# T0955, T0953s2-D1, T0957s1-D2, T1019s1-D1, T0990-D1, T1006-D1, T0963-D2, T0991
+protein_names = ['T0955', 'T0953s2', 'T0957s1', 'T1019s1', 'T0990', 'T1006','T0963', 'T0991', 'T0986s2']
+dist_start = [1, 2, 38, 1,  1, 1, 40, 1, 1]
+dist_end = [41, 45, 91, 58, 76, 77, 121, 118, 155]
 
+protein_names = ['T0955']
+dist_start = [1]
+dist_end = [41]
+
+strip_r_group = True
 # misc
-data_filename = 'T0955_cyc' + str(cycles) + '_ite' + str(iterations) + '_seg' +str(segment_sizes) + '_data.json'
-mat_filename = 'T0955_cyc' + str(cycles) + '_ite' + str(iterations) + '_seg' +str(segment_sizes) + '_data.mat'
+# data_filename = 'T0955_cyc' + str(cycles) + '_ite' + str(iterations) + '_seg' +str(segment_sizes) + '_fpgadata.json'
+# mat_filename = 'T0955_cyc' + str(cycles) + '_ite' + str(iterations) + '_seg' +str(segment_sizes) + '_fpgadata.mat'
 
-server_ipv4_address = 'ubuntu@'
+# server_ipv4_address = 'ubuntu@'
 ##############################################################################################
 
+def strip_rgroup(xyz_coords):
+    pass
 
-# All values to be sent to GPU stored in r
-r={}
+for ind, name in enumerate(protein_names):
+    dist_filename = r'../{}/contacts/{}.pickle'.format(name, name) #C:/Users/alasg/Documents/GitHub/Molecular_simulations/md/AlphaFold
+    initial_state_pdb_filename = r'../{}/{}_unfolded.pdb'.format(name, name)
+    
+    # All values to be sent to fpga stored in r
+    r={}
 
-with open(dist_filename, 'rb') as infile:
-    new_dict = pickle.load(infile, encoding='latin1')
-r['distogram'] = new_dict['probs']
-# else:
-    # r['distogram'] = new_dict['probs'][dist_start - 1:dist_end, dist_start - 1:dist_end]
+    with open(dist_filename, 'rb') as infile:
+        new_dict = pickle.load(infile, encoding='latin1')
+    # r['distogram'] = new_dict['probs']
+    r['distogram'] = new_dict['probs'][dist_start[ind] - 1:dist_end[ind], dist_start[ind] - 1:dist_end[ind]]
+    default_traj = md.load(initial_state_pdb_filename)
+    
+    # strip r group
+    if strip_r_group:
+        atom_names_to_keep = ['CA', 'CB', 'N', 'C']
+        trunc_topology = default_traj.topology.copy()
+        table, bonds = default_traj.topology.to_dataframe()
+        deleted_atoms = []
+        for i in range(default_traj.n_atoms - 1, -1, -1):
+            if str(table['name'][i]) not in atom_names_to_keep:
+                trunc_topology.delete_atom_by_index(i)
+                deleted_atoms.append(i)
+        table, bonds = trunc_topology.to_dataframe()
+        index = table.index
+        positions = np.squeeze(np.array(default_traj.xyz[0, :, :]))
+        # remove deleted atoms from trajectory
+        molecule = []
+        for i in range(len(positions)):
+            if i not in deleted_atoms:
+                molecule.append(positions[i, :])
+        r['molecule_xyz'] = np.array(molecule)
+        r['topology'] = trunc_topology
+    else:
+        r['molecule_xyz'] = np.squeeze(np.array(default_traj.xyz[0, :, :]))
+        r['topology'] = default_traj.topology
 
-default_traj = md.load(initial_state_pdb_filename)
-topology = default_traj.topology
-r['topology'] = topology
-table, bonds = topology.to_dataframe()
-index = table.index
-r['molecule_xyz'] = np.squeeze(np.array(default_traj.xyz[0, :, :]))
-r['n_residues'] = default_traj.n_residues
-r['n_atoms'] = default_traj.n_atoms
-r['CA'] = np.array(index[table['name'] == 'CA'])  # folding
-r['CB'] =  np.array(index[table['name'] == 'CB'])  # energy calculation
-r['N'] =  np.array(index[table['name'] == 'N'])
-r['C'] =  np.array(index[table['name'] == 'C'])
-r['O'] =  np.array(index[table['name'] == 'O'])  # phi
-r['H'] =  np.array(index[table['name'] == 'H'])  # psi
-r['noise_var_range'] = np.array([10,1])
-r['beta_cycles'] = np.array([1, cycles])
-r['beta'] = 1
-r['cycles']=cycles
-r['segment_sizes'] = np.array(segment_sizes)
-r['iterations'] = np.array(iterations)
+    r['n_residues'] = default_traj.n_residues
+    table, bonds = r['topology'].to_dataframe()
+    index = table.index
+    r['CA'] = np.array(index[table['name'] == 'CA'])  # folding
+    r['N'] = np.array(index[table['name'] == 'N'])
+    r['C'] = np.array(index[table['name'] == 'C'])
+    r['O'] =  np.array(index[table['name'] == 'O'])  # phi
+    r['H'] =  np.array(index[table['name'] == 'H'])  # psi
+    r['CB'] = []
+    res_start= []
+    start_resseq = table['resSeq'][0]
+    end_resseq = table['resSeq'].values[-1]
+    for i in range(start_resseq, end_resseq+1, 1):
+        indz =  np.array(index[table['resSeq'] == i])
+        res_start.append(indz[0])
+    for i in range(r['n_residues']):
+        if table['resName'][res_start[i]] == 'GLY':
+            r['CB'].append(res_start[i]+1)
+        else:
+            r['CB'].append(res_start[i]+3)
+    r['n_atoms'] = len(r['molecule_xyz'])
+    r['noise_var_range'] = np.array([10,1])
+    r['beta_cycles'] = np.array([1, cycles])
+    r['beta'] = 1
+    r['cycles']=cycles
+    r['segment_sizes'] = np.array(segment_sizes)
+    r['iterations'] = np.array(iterations)
 
-jd.save(r, 'fpga_run_data/'+data_filename)
-savemat(file_name='fpga_run_data/'+ mat_filename, mdict=r)
+   
+    # uncomment below lines to get new data for GPU to run!
+    print('n_residues: ', r['n_residues'])
+    flattened_distogram = [] #np.zeros(1,r['n_residues']*r['n_residues'] * 64)
+    for i in range(r['n_residues']):
+        for j in range(r['n_residues']):
+            flattened_distogram += list(r['distogram'][i][j][:])
 
+    flattened_distogram = np.array(flattened_distogram)
+    print('flattened distogram shape: ', flattened_distogram.shape)
 
-# flatten out the distogram into a 1d string to make reading easier in c++ (and sending to fpga easier). 
-# I will have n_residue info to read it properly in the fpga
+    flattened_log_distogram = np.log(flattened_distogram)
 
-# uncomment below lines to get new data for GPU to run!
+    flattened_state = []
+    for mol in r['molecule_xyz']:
+        for axis in mol:
+            flattened_state += [axis]
 
-
-flattened_distogram = [] #np.zeros(1,r['n_residues']*r['n_residues'] * 64)
-for i in range(r['n_residues']):
-    for j in range(r['n_residues']):
-        flattened_distogram += list(r['distogram'][i][j][:])
-
-flattened_distogram = np.array(flattened_distogram)
-print(f'flattened_distogram shape: {flattened_distogram.shape}')
-
-flattened_log_distogram = np.log(flattened_distogram)
-print(f"r[CA].shape: {r['CA'].shape}")
-
-flattened_state = []
-for mol in r['molecule_xyz']:
-    for axis in mol:
-        flattened_state += [axis]
-
-print("initial state length: ", np.array(flattened_state).shape)
-
+    print("State length: ", np.array(flattened_state).shape)
 
 
-max_dist_weight = np.amax(flattened_distogram)
-min_dist_weight = np.amin(flattened_distogram)
-
-max_dist_logweight = np.amax(flattened_log_distogram)
-min_dist_logweight = np.amin(flattened_log_distogram)
-
-max_state = np.amax(flattened_state)
-min_state = np.amin(flattened_state)
-
-# print(max_dist_weight)
-# print(min_dist_weight)
-# print(max_dist_logweight)
-# print(min_dist_logweight)
-# print(max_state)
-# print(min_state)
-
-
-# convert ind array elements to 16 bit sequences
-r['CA_bin'], r['CB_bin'], r['C_bin'], r['N_bin'], r['O_bin'] = [], [], [], [], []
-
-for ind in r['CA']:
-    binval = bin(ind)[2:]    
-    binval = '0'*(16-len(binval)) + binval
-    r['CA_bin'].append(binval)
-for ind in r['CB']:
-    binval = bin(ind)[2:]    
-    binval = '0'*(16-len(binval)) + binval
-    r['CB_bin'].append(binval)
-for ind in r['C']:
-    binval = bin(ind)[2:]    
-    binval = '0'*(16-len(binval)) + binval
-    r['C_bin'].append(binval)
-for ind in r['N']:
-    binval = bin(ind)[2:]    
-    binval = '0'*(16-len(binval)) + binval
-    r['N_bin'].append(binval)
-for ind in r['O']:
-    binval = bin(ind)[2:]    
-    binval = '0'*(16-len(binval)) + binval
-    r['O_bin'].append(binval)
-
-# convert the dist and state to 32 bit fixed point sequences where [+-, 5 integer bits, 25 fractional bits]
-flattened_distogram_bin, flattened_log_distogram_bin, flattened_state_bin  = [], [], []
-for w in flattened_distogram:
-    x = Fxp(w, signed=True, n_word=32, n_frac=25)
-    flattened_distogram_bin.append(x.bin())
-for w in flattened_log_distogram:
-    x = Fxp(w, signed=True, n_word=32, n_frac=25)
-    flattened_log_distogram_bin.append(x.bin())
-for w in flattened_state:
-    x = Fxp(w, signed=True, n_word=32, n_frac=25)
-    flattened_state_bin.append(x.bin())
-
-# write to csv file, one row per variable
-f = open(data_filename, 'w')
-writer = csv.writer(f)
-writer.writerow(flattened_distogram_bin)
-writer.writerow(flattened_log_distogram_bin)
-writer.writerow(flattened_state_bin)
-writer.writerow(r['CA_bin'])
-writer.writerow(r['CB_bin'])
-writer.writerow(r['C_bin'])
-writer.writerow(r['N_bin'])
-writer.writerow(r['O_bin'])
-f.close()
+    # write to csv file, one row per variable
+    stri = r'C:\Users\Anirudh Ghantasala\Documents\GitHub\Molecular_simulations\md\AlphaFold\fpga_emulator\fpga_run_data\gpudata_{}.csv'.format(name)
+    f = open(stri, 'w')
+    writer = csv.writer(f)
+    writer.writerow(flattened_distogram)
+    writer.writerow(flattened_state)
+    writer.writerow(r['CA'])
+    writer.writerow(r['CB'])
+    writer.writerow(r['C'])
+    writer.writerow(r['N'])
+    # writer.writerow(r['O'])
+    # writer.writerow(r['H'])
+    f.close()
